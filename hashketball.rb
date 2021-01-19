b32ddd61ee5f10ffba1657cbeb4e8ec8ace56943
Liz Burke <liz.burke.chi@gmail.com>
2021-01-19 18:56:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,29 @@
-# Write your code here!
+require "pry"
+
+def game_hash
+  {
+  :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black","White"],
+      :players => [
+        {player_name: "Alan Anderrson", number: "0", shoe: "16", points: "22", rebounds: "12", assists: "12", steals: "3",  blocks: "1", slam_dunks: "1" },
+        {player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "",  blocks: "", slam_dunks: "" },
+        {player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "",  blocks: "", slam_dunks: "" },
+        {player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "",  blocks: "", slam_dunks: "" },
+        {player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "",  blocks: "", slam_dunks: "" }
+      ]
+      },
+
+  :away => {
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
+      :players => [
+        {player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "",  blocks: "", slam_dunks: "" },
+        {player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "",  blocks: "", slam_dunks: "" },
+        {player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "",  blocks: "", slam_dunks: "" },
+        {player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "",  blocks: "", slam_dunks: "" },
+        {player_name: "", number: "", shoe: "", points: "", rebounds: "", assists: "", steals: "",  blocks: "", slam_dunks: "" }
+      ]
+    }
+    }
+end
